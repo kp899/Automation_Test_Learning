2c0a97eaa169c6cbf6b2ff9d89418ad0650944ad
Komalpreet Maan <komalmann899@gmail.com>
2021-11-18 15:20:18 +1000

    Update my_first_test.js

--- Project1/cypress/integration/my_first_test.js
+++ Project1/cypress/integration/my_first_test.js
@@ -5,12 +5,18 @@ it('google test', function(){
     cy.get('.gLFyf').type('Spotify{enter}')
 })
 
-it.only('login vist', function(){
+it('login vist', function(){
     cy.visit('https://opensource-demo.orangehrmlive.com/')
     cy.get('#divUsername > .form-hint').type('Admin')
     cy.get('#txtPassword').type('admin123')
     cy.get('#btnLogin').click()
     cy.get('#menu_admin_viewAdminModule > b').click()
-    cy.get('#menu_admin_UserManagement').click()
-    
+    cy.get('#menu_admin_Organization').click()
+})
+
+it.only('Translink app', function(){
+    cy.visit('https://translink.com.au/')
+    cy.get('#Start').type('South Bank')
+    cy.get(':nth-child(3) > .input-group > .input-group-addon > .detect-location-icon').click()
+    cy.get('.btn-loading').click()
 })
