dc3c7aaa17d1cb7dde2057b4f29cca30ae1b5690
Komalpreet Maan <komalmann899@gmail.com>
2021-11-25 13:03:39 +1000

    Update Assertions.ts

--- Project1/cypress/integration/Assertions.ts
+++ Project1/cypress/integration/Assertions.ts
@@ -5,6 +5,22 @@ it('Learning Assertions',function(){
         .should('contain', 'Button')
         .should('have.class','query-btn')
         .should('be.visible')
+
+
     cy.get('#query-btn').invoke('attr', 'id')   
         .should('equal','query-btn') 
+
+    cy.get('#query-btn')
+        .should('contain', 'Button')
+        .and('have.class', 'query-btn')
+
+    //Explicit assertion
+    //expect
+    expect(true).to.be.true
+
+    //let name = 'Komal'
+    //expect(name).to.be.('Komal')
+
+    assert.equal(4,4, 'Not Equal')
+    assert.strictEqual(4,4, 'Not Strictly Equal')
 })
